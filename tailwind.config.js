75c84c6c4817b77ba23bd2c7e60cc9522f86438f
Matheus <matheusramosbi.13@gmail.com>
2023-12-15 18:32:49 -0300

    add `bg` & start tasks section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         sans: ["DM Sans", "sans-serif"],
       },
+      backgroundImage: {
+        "bg-body": "/img/bg-body.svg",
+        "bg-lofi": "/img/bg-lofi.png",
+      },
+    },
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "50%": "50%",
+      "100%": "100%",
     },
   },
   plugins: [],
