a07adc4a48f0313e789191a19b67aa289e168daa
Matheus <matheusramosbi.13@gmail.com>
2023-12-27 22:35:39 -0300

    add `utils.js` file

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+// utils.js
+const modalityButtons = document.querySelectorAll(".modality");
+
+modalityButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    modalityButtons.forEach((b) => b.classList.remove("active-button"));
+    button.classList.add("active-button");
+    document.title = button.innerText + " - Pomodoro Timer";
+  });
+});
